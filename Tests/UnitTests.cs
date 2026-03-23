273e0bcf606bc75124e95ac0c3e3e7a5a4104679
Ethan Ramaekers <ethanramaekers@Ethans-MacBook-Pro-1074.local>
2026-03-23 15:37:14 -0500

    Unit tests added; one failure

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,15 +4,15 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Ramaekers()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("5", "7"));
+		Assert.AreEqual(12, Program.Add("8", "7"));
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Ramaekers()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,10 +20,124 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Ramaekers()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+	
 }
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Ramaekers()
+	{
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(4, Program.Subtract("9", "5"));
+		Assert.AreEqual(-2, Program.Subtract("5", "7"));
+	}
+
+	[TestMethod]
+	public void Subtract_Invalid_Ramaekers()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Subtract_Null_Ramaekers()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
+
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Multiply_Valid_Ramaekers()
+	{
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
+		Assert.AreEqual(45, Program.Multiply("9", "5"));
+		Assert.AreEqual(35, Program.Multiply("5", "7"));
+	}
+
+	[TestMethod]
+	public void Multiply_Invalid_Ramaekers()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Multiply_Null_Ramaekers()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid_Ramaekers()
+	{
+		Assert.AreEqual(2, Program.Divide("4", "2"));
+		Assert.AreEqual(5, Program.Divide("10", "2"));
+		Assert.AreEqual(2, Program.Divide("14", "7"));
+	}
+
+	[TestMethod]
+	public void Divide_Invalid_Ramaekers()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Divide_Null_Ramaekers()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
+
+[TestClass]
+public class PowerTests
+{
+	[TestMethod]
+	public void Power_Valid_Ramaekers()
+	{
+		Assert.AreEqual(8, Program.Power("2", "3"));
+		Assert.AreEqual(16, Program.Power("4", "2"));
+		Assert.AreEqual(25, Program.Power("5", "2"));
+	}
+
+	[TestMethod]
+	public void Power_Invalid_Ramaekers()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+	}
+
+	[TestMethod]
+	public void Power_Null_Ramaekers()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+	}
+}
+
