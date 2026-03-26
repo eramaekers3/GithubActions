e1f8e859bb71226cfe01edda7d13d11e8e66c5d3
Ethan Ramaekers <ethanramaekers@Ethans-MacBook-Pro-1074.local>
2026-03-25 22:59:42 -0500

    Fix failing unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -8,7 +8,7 @@ public class Addition
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("8", "7"));
+		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
 
 	[TestMethod]
